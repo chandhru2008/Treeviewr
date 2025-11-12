e95cef0fd36bc51f9558a48054987d9038a69a1d
mohamedj <mohamedj0910@gmail.com>
2025-11-12 22:04:17 +0530

    feat(theme): Implemented light and dark theme mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import Header from "./components/Header";
 import HierarchyTree from "./components/HierarchyTree";
 import { hierarchyData } from "./data/sampledata";
 
 function App() {
   return (
-    <div className="min-h-screen p-10">
-      <h1 className="text-3xl font-bold mb-6 text-gray-800">Hierarchy Layout</h1>
-
-      <div className="bg-white p-6 shadow rounded-xl">
+    <div className="min-h-screen p-10 bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
+      <Header />
+      <div className="bg-white dark:bg-gray-800 p-6 shadow rounded-xl border border-gray-200 dark:border-gray-700">
         <HierarchyTree data={hierarchyData} />
       </div>
     </div>
--- src/components/HierarchySearch.tsx
+++ src/components/HierarchySearch.tsx
@@ -3,19 +3,17 @@ import type { HierarchySearchProps } from "../types/types";
 
 const HierarchySearch: React.FC<HierarchySearchProps> = ({ onSearch }) => {
   const [query, setQuery] = useState("");
-
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    function handleKeyDown(e:KeyboardEvent) {
-      if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'k') {
+    function handleKeyDown(e: KeyboardEvent) {
+      if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "k") {
         e.preventDefault();
         inputRef.current?.focus();
       }
     }
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => window.removeEventListener('keydown', handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
   }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,7 +30,13 @@ const HierarchySearch: React.FC<HierarchySearchProps> = ({ onSearch }) => {
         ref={inputRef}
         onChange={handleChange}
         placeholder="Search (Press Ctrl + K)..."
-        className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="w-full px-3 py-2 border rounded-md
+          focus:outline-none focus:ring-2 focus:ring-blue-400
+          bg-gray-50 dark:bg-gray-700
+          border-gray-300 dark:border-gray-600
+          text-gray-900 dark:text-gray-100
+          placeholder-gray-500 dark:placeholder-gray-400
+        "
       />
     </div>
   );
